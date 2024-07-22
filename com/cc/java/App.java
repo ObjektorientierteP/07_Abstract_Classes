dd3b24f982cabb8a6359a7cd70cc4802fa75c614
Arian <a.hormozfar@yahoo.de>
2024-07-22 11:23:30 +0200

    2. Iteration: Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -19,16 +19,25 @@ public class App {
         // Berechnung
         output("------  Differenzen ------");
         output("Diff. r-t :" + areaDiff(rectangle, triangle));
-        output("Diff. r-t :" + areaDiff(rectangle, circle));
+        output("Diff. r-c :" + areaDiff(rectangle, circle));
+        output("Diff. c-r: " + areaDiff(circle,rectangle));
+        output("Diff. c-t: " + areaDiff(circle, triangle));
+        output("Diff. t-t: " + areaDiff(triangle, triangle));
 
 
     }
 
-    private static double  areaDiff(Rectangle r, Triangle t){
-        return r.area() - t.area();
-    }
-    private static double  areaDiff(Rectangle r, Circle c){
-        return r.area() - c.area();
+    // 1. Iteration
+    // private static double  areaDiff(Rectangle r, Triangle t){
+    //     return r.area() - t.area();
+    // }
+    // private static double  areaDiff(Rectangle r, Circle c){
+    //     return r.area() - c.area();
+    // }
+
+    // 2. Iteration
+    private static double  areaDiff(Shape s1, Shape s2){
+        return s1.area() - s2.area();
     }
 
     private static void output(String outputStr) {
